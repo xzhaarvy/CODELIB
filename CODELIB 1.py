66119761792bd65ef7ac06d21d4f252d90ac7721
Zxrfhy <98753334+Zxrfhy@users.noreply.github.com>
2025-01-29 17:57:26 +0700

    PELATIHAN PYTHON 1
    simple code

---
+++ CODELIB 1.py
@@ -0,0 +1,14 @@
+#program dimulai dengan sambutan
+print ("Hari ini","adalah","hari Rabu",sep="*",end="+")
+print ("Gong Xie Fa Chai...")
+
+#user harus menginput data
+nama = input("Masukan nama anda:")
+print ("Nama anda adalah",nama)
+fullname = "Fajar Adriansyah"
+surname = "Fajar"
+x123 = "Fajar Adriansyah"
+
+#menghitung panjang nama yang di input
+panjang = len(nama) 
+print("panjang string fullname adalah",panjang)
